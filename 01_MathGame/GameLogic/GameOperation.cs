7417a27217d6090b3464f9dd9239c60f2c33d091
anarusu <rusu.annaa@gmail.com>
2025-06-09 15:44:14 +0300

    Refactor: move MathGame files to project root

---
+++ 01_MathGame/GameLogic/GameOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathGame.Models;
+
+namespace MathGame.GameLogic
+{
+    public abstract class GameOperation
+    {
+        public abstract Question GenerateQuestion(DifficultyLevel difficulty);
+       
+    }
+}
